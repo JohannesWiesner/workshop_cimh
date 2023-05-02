a14e5941ad7b27ab3246a880d0cbee68723ec47b
JohannesWiesner <joh.wiesner@gmail.com>
2023-05-02 17:34:12 +0200

    map directories on latest Dockerfile, add nilearn to dependencies

--- run_docker.sh
+++ run_docker.sh
@@ -2,10 +2,10 @@
 
 run_docker(){
     docker run -ti --rm \
-    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing/code:/code  \
-    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing/data:/data \
-    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing//cache:/cache \
-    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing//output:/output \
+    -v /home/johannes.wiesner/work/repos/workshop_cimh/workshop/courses/nipype/code:/code  \
+    -v /home/johannes.wiesner/work/repos/workshop_cimh/workshop/courses/nipype/data:/data \
+    -v /home/johannes.wiesner/work/repos/workshop_cimh/workshop/courses/nipype/cache:/cache \
+    -v /home/johannes.wiesner/work/repos/workshop_cimh/workshop/courses/nipype/output:/output \
     -p 8888:8888 \
     workshop_cimh:test
 }
