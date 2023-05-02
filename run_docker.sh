1f3e26685dd5242cb602057e3cf67f18e7dc16b7
JohannesWiesner <joh.wiesner@gmail.com>
2023-05-02 14:09:14 +0200

    switched to root user

--- run_docker.sh
+++ run_docker.sh
@@ -2,10 +2,10 @@
 
 run_docker(){
     docker run -ti --rm \
-    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing/code:/home/csp/code  \
-    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing/data:/home/csp/data \
-    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing//cache:/home/csp/cache \
-    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing//output:/home/csp/output \
+    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing/code:/code  \
+    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing/data:/data \
+    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing//cache:/cache \
+    -v /home/johannes.wiesner/work/repos/csp_neurodocker/testing//output:/output \
     -p 8888:8888 \
     workshop_cimh:test
 }
