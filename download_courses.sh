34f6b33ac0318ec9a841feb318d4e7d2d3806f9b
JohannesWiesner <joh.wiesner@gmail.com>
2023-07-11 13:46:49 +0200

    worked on introduction to Nodes

--- download_courses.sh
+++ download_courses.sh
@@ -11,8 +11,8 @@ curl -kLSs https://github.com/JohannesWiesner/workshop_cimh/archive/master.tar.g
 # the nipype/data folder. Then unzip the two because neuroimaging software like SPM can't handle zipped files.
 ######################################################################################################################################
 
-curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004302/sub-02/anat/sub-02_T1w.nii.gz?versionId=93eQ.AwPcMeccJAT3sO9otYv4A_WH3Bj -o courses/nipype/data/functional.nii.gz
-curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004302/sub-02/func/sub-02_task-speech_bold.nii.gz?versionId=fhx4kutoEiWzbik8u7lC0eqKKFkJ61HK -o courses/nipype/data/anatomical.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004302/sub-02/anat/sub-02_T1w.nii.gz?versionId=93eQ.AwPcMeccJAT3sO9otYv4A_WH3Bj -o courses/nipype/data/anatomical.nii.gz
+curl --create-dirs https://s3.amazonaws.com/openneuro.org/ds004302/sub-02/func/sub-02_task-speech_bold.nii.gz?versionId=fhx4kutoEiWzbik8u7lC0eqKKFkJ61HK -o courses/nipype/data/functional.nii.gz
 gunzip courses/nipype/data/functional.nii.gz
 gunzip courses/nipype/data/anatomical.nii.gz
 
